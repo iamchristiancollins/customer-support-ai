d062259fc92be3a70bc77d7b6adce4f067121b63
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-08-07 10:58:28 -0500

    restructure for auth

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const apiKey = process.env.FIREBASE_API_KEY;
-const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
-const projectId = process.env.FIREBASE_PROJECT_ID;
-const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
-const appId = process.env.FIREBASE_APP_ID;
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -25,4 +25,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
